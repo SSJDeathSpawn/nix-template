647cb8b66b475197c92875235e645ea2be510c7d
SSJDeathSpawn <adikesavan28@gmail.com>
2025-04-08 21:58:57 +0530

    Added python template

---
+++ python/flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Python";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=release-24.11";
+  };
+
+  outputs = { self, nixpkgs } : 
+  let 
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  in
+  {
+    
+    devShells.x86_64-linux.default = pkgs.mkShell {
+      packages = with pkgs; [
+          python3
+      ];
+    };
+
+  };
+}
